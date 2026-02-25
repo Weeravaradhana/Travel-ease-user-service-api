7240345e699e1ce967a792a6750bece27024f7b9
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-02-25 14:44:49 +0530

    `feat: enhance ObjectMapper with FileDataExtractor, mapToUserResponse, and extractRoles methods`

--- src/main/java/com/travel_ease/hotel_system/util/ObjectMapper.java
+++ src/main/java/com/travel_ease/hotel_system/util/ObjectMapper.java
@@ -1,14 +1,23 @@
 package com.travel_ease.hotel_system.util;
 
 import com.travel_ease.hotel_system.dto.request.UserRequestDto;
+import com.travel_ease.hotel_system.dto.response.UserResponseDto;
+import com.travel_ease.hotel_system.entity.User;
+import lombok.RequiredArgsConstructor;
 import org.keycloak.representations.idm.CredentialRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Component
+@RequiredArgsConstructor
 public class ObjectMapper {
+
+    private final FileDataExtractor fileDataExtractor;
+
+
     public UserRepresentation mapUserRepo(UserRequestDto dto, boolean isEmailVerified , boolean isEnable) {
         UserRepresentation user = new UserRepresentation();
         user.setEmail(dto.getEmail());
@@ -24,5 +33,29 @@ public class ObjectMapper {
         credList.add(cred);
         user.setCredentials(credList);
         return user;
+    }
+
+    public List<String> extractRoles(Map<String, Object> tokenData) {
+        Map<String, Object> realmAccess = (Map<String, Object>) tokenData.get("realm_access");
+        if (realmAccess != null && realmAccess.containsKey("roles")) {
+            return (List<String>) realmAccess.get("roles");
+        }
+        return List.of();
+    }
+
+    public UserResponseDto mapToUserResponse(User user) {
+        return UserResponseDto.builder()
+                .id(user.getId())
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .email(user.getEmail())
+                .contact(user.getContact())
+                .role(user.getRole())
+                .emailVerified(user.isEmailVerified())
+                .status(user.getStatus())
+                .avatarUrl(user.getUserAvatar() != null ? fileDataExtractor.byteArrayToString(user.getUserAvatar().getResourceUrl()) : null)
+                .createdAt(user.getCreatedAt())
+                .updatedAt(user.getUpdatedAt())
+                .build();
     }
 }
