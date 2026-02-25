8a06593ba683612d0db147d0231d025be0bba13b
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-02-25 11:23:51 +0530

    feat: Add OtpRepository interface
    - Created OtpRepository extending JpaRepository for Otp entity
    - Supports CRUD operations and basic query methods for OTP management

---
+++ src/main/java/com/travel_ease/hotel_system/repository/OtpRepository.java
@@ -0,0 +1,8 @@
+package com.travel_ease.hotel_system.repository;
+
+import com.travel_ease.hotel_system.entity.Otp;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+
+public interface OtpRepository extends JpaRepository<Otp, UUID> {
+}
